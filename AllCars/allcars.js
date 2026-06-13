6cf551495635a3f047123fbe5011a026c7a2c113
Petar <petardonevski21@gmail.com>
2026-06-13 04:29:57 +0200

    update project

--- AllCars/allcars.js
+++ AllCars/allcars.js
@@ -1,17 +1,17 @@
 // Податоци за автомобилите (ПОПРАВЕНА ПАТЕКА ДО ПОРШЕТО)
 const cars = [
-    { name: "Porsche 911 Carrera", price: 900, isNew: true, img: "AllCars sliki/porrse.jpg", link: "../Porsche911Carrera/porse911.html" },
-    { name: "Mercedes SL63 Cabrio", price: 800, isNew: true, img: "AllCars sliki/merc.png" },
-    { name: "McLaren 765LT", price: 1600, isNew: true, img: "AllCars sliki/mcclaren.jpg" },
-    { name: "Mercedes S63 Cabrio", price: 700, isNew: false, img: "AllCars sliki/merr.jpg"  },
-    { name: "BMW M4 Cabrio", price: 400, isNew: false, img: "AllCars sliki/m4.jpg"  },
-    { name: "Ferrari 488 Spider", price: 1100, isNew: false, img: "AllCars sliki/ferrari.png"  },
-    { name: "Rolls-Royce Dawn", price: 1700, isNew: false, img: "AllCars sliki/rolls.png"   },
-    { name: "Range Rover Evoque Cabrio", price: 300, isNew: false, img:  "AllCars sliki/range.jpg"   },
-    { name: "Lamborghini Huracan", price: 1500, isNew: false, img: "AllCars sliki/lambo.jpg"  },
-    { name: "Ferrari 812 GTS", price: 1700, isNew: false, img:  "AllCars sliki/modern.jpg." },
-    { name: "BMW M8 Cabrio", price: 800, isNew: false, img:   "AllCars sliki/m8.jpg."  },
-    { name: "Ferrari SF90 Spider", price: 2000, isNew: false, img:  "AllCars sliki/download.jpg."   }
+{ name: "Porsche 911 Carrera", price: 900, isNew: true, img: "AllCars sliki/porrse.jpg", link: "/Porsche911Carrera/porse911.html" },
+{ name: "Mercedes SL63 Cabrio", price: 800, isNew: true, img: "AllCars sliki/merc.png" },
+{ name: "McLaren 765LT", price: 1600, isNew: true, img: "AllCars sliki/mcclaren.jpg" },
+{ name: "Mercedes S63 Cabrio", price: 700, isNew: false, img: "AllCars sliki/merr.jpg" },
+{ name: "BMW M4 Cabrio", price: 400, isNew: false, img: "AllCars sliki/m4.jpg" },
+{ name: "Ferrari 488 Spider", price: 1100, isNew: false, img: "AllCars sliki/ferrari.png" },
+{ name: "Rolls-Royce Dawn", price: 1700, isNew: false, img: "AllCars sliki/rolls.png" },
+{ name: "Range Rover Evoque Cabrio", price: 300, isNew: false, img: "AllCars sliki/range.jpg" },
+{ name: "Lamborghini Huracan", price: 1500, isNew: false, img: "AllCars sliki/lambo.jpg" },
+{ name: "Ferrari 812 GTS", price: 1700, isNew: false, img: "AllCars sliki/modern.jpg" },
+{ name: "BMW M8 Cabrio", price: 800, isNew: false, img: "AllCars sliki/m8.jpg" },
+{ name: "Ferrari SF90 Spider", price: 2000, isNew: false, img: "AllCars sliki/download.jpg" }
 ];
 
 // Функција за прикажување на автомобилите во HTML
